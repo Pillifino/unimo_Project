15b667c206642790ac360fe41768fff212fc20ce
Pillifino <dinodizon073100@gmail.com>
2022-09-08 00:37:38 -0700

    testing deploy

--- README.md
+++
@@ -1,1 +0,0 @@
-# unimo_Project
---
+++ unimo-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  theme,
+} from '@chakra-ui/react';
+
+// import { ColorModeSwitcher } from './ColorModeSwitcher';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+
+    </ChakraProvider>
+  );
+}
+
+export default App;
