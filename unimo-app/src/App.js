dea89103f61d7533387f6c1b6462f1049f420fcf
Pillifino <dinodizon073100@gmail.com>
2022-09-08 10:26:58 -0700

    imported font style

--- unimo-app/src/App.js
+++ unimo-app/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {
   ChakraProvider,
-  theme,
 } from '@chakra-ui/react';
 
 // import { ColorModeSwitcher } from './ColorModeSwitcher';
 import Container from './Components/Container/container';
+import myNewTheme from './Styles/theme';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={myNewTheme}>
       <Container/>
     </ChakraProvider>
   );
