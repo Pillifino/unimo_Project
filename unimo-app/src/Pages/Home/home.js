8e6258cd19cef668601efe08b29139197f268944
Pillifino <dinodizon073100@gmail.com>
2022-09-08 11:32:30 -0700

    home styling

--- unimo-app/src/Pages/Home/home.js
+++ unimo-app/src/Pages/Home/home.js
@@ -1,25 +1,40 @@
-import React from 'react'
+import React from 'react';
 
 // Nav
-import Nav from '../../Components/Navbar/nav'
+import Nav from '../../Components/Navbar/nav';
 
-import { Flex, Text } from "@chakra-ui/react"
+import { Flex, Text, Input } from '@chakra-ui/react';
 
 function Home() {
   return (
     <div>
-      <Nav/>
-      <Flex
-      bg="#E6E6E6"
-      h="100vh"
-      w="100vw"
+      <Nav />
+      <Flex justify="center"
+      bg="bg"
       >
-      <Text
-      color="black"
-      >Home</Text>
+        <Flex bg="bg" h="100vh" w="85%" flexDirection="column">
+          <Flex
+            // bg="green"
+            w="100%"
+            h="10%"
+            align="center"
+            justify="space-between"
+          >
+            <Text color="black" fontSize="1.5em">
+              Explore
+            </Text>
+            <Input w="50%" placeholder="Search" color="White" size="md" variant="Filled" focusBorderColor='black'/>
+          </Flex>
+          <Flex 
+          // bg="orange" 
+          w="100%" 
+          h="90%">
+            {/*  */}
+          </Flex>
+        </Flex>
       </Flex>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
